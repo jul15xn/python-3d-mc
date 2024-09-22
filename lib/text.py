d3dedfe8d2630cfbedaa8ff83d5458135309a7d9
Jul1xn <julian.verwoerd@pantronic.nl>
2024-09-22 18:47:37 +0200

    Init

--- lib/text.py
+++
@@ -1,6 +0,0 @@
-from lib import window
-import pygame
-
-def draw_text(pos, text, fontsize, color = pygame.Color("coral")):
-    text_render = window.WIN_FONT.render(str(text), 0, color)
-    window.GAME_WIN.blit(text_render, pos)
