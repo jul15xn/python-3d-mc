0f56c38be845f1a32ca3c58da0e8866ae7df5f0f
Jul15xn <julian.verwoerd@pantronic.nl>
2024-09-23 13:01:42 +0200

    Transformation update

--- model.py
+++ model.py
@@ -4,8 +4,11 @@ import pygame as pg
 import moderngl as mgl
 
 class BaseModel:
-    def __init__(self, app, vao_name, tex_id):
+    def __init__(self, app, vao_name, tex_id, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
         self.app = app
+        self.pos = pos
+        self.rot = glm.vec3([glm.radians(a) for a in rot])
+        self.scale = scale
         self.m_model = self.get_model_matrix()
         self.tex_id = tex_id
         self.vao = app.mesh.vao.vaos[vao_name]
@@ -17,6 +20,13 @@ class BaseModel:
 
     def get_model_matrix(self):
         m_model = glm.mat4()
+        m_model = glm.translate(m_model, self.pos)
+
+        m_model = glm.rotate(m_model, self.rot.x, glm.vec3(1, 0, 0))
+        m_model = glm.rotate(m_model, self.rot.y, glm.vec3(0, 1, 0))
+        m_model = glm.rotate(m_model, self.rot.z, glm.vec3(0, 0, 1))
+
+        m_model = glm.scale(m_model, self.scale)
         return m_model
     
     def render(self):
@@ -24,8 +34,8 @@ class BaseModel:
         self.vao.render()
 
 class Cube(BaseModel):
-    def __init__(self, app, vao_name='cube', tex_id=0):
-        super().__init__(app, vao_name, tex_id)    
+    def __init__(self, app, vao_name='cube', tex_id=0, pos=(0, 0, 0), rot=(0, 0, 0), scale=(1, 1, 1)):
+        super().__init__(app, vao_name, tex_id, pos, rot, scale)    
         self.on_init()
 
     def update(self):
