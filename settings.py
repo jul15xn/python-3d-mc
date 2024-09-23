4d1398f2811e5ffbfae59b54b80b509d9b5bdb53
Jul1xn <julian.verwoerd@pantronic.nl>
2024-09-23 18:25:24 +0200

    Base setup

--- settings.py
+++ settings.py
@@ -6,5 +6,20 @@ import math
 # Screen resolution
 WIN_RES = glm.vec2(1600, 900)
 
+# camera
+ASPECT_RATIO = WIN_RES.x / WIN_RES.y
+FOV_DEG = 60
+V_FOV = glm.radians(FOV_DEG)
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)
+NEAR = 0.1
+FAR = 2000.0
+PITCH_MAX = glm.radians(89)
+
+#player
+PLAYER_SPEED = 0.005
+PLAYER_ROT_SPEED = 0.003
+PLAYER_POS = glm.vec3(0, 0, 1)
+MOUSE_SENSITIVITY = 0.002
+
 # Color
 BG_COLOR = glm.vec3(0.1, 0.16, 0.25)
